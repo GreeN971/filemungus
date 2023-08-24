f7d9a27c96e70eab61b07b6d9491f8db3b136bbd
GreeN971 <offgreen@seznam.cz>
2023-08-24 17:09:29 +0200

    Fix linux local wxWidgets(cmake)

--- cmake/ProjectLibs.cmake
+++ cmake/ProjectLibs.cmake
@@ -1,16 +1,17 @@
-if(NOT WIN32)
-    set(CPM_LOCAL_PACKAGES_ONLY True)
-    set(CPM_USE_LOCAL_PACKAGES True)
-endif()
+find_package(wxWidgets 3.2.0 REQUIRED COMPONENTS core base std richtext propgrid adv)
+if(NOT wxWidgets_FOUND)
+    set(CPM_USE_LOCAL_PACKAGES False)
 
-set(CPM_SOURCE_CACHE ${CMAKE_CURRENT_LIST_DIR}/third_party/CPM)
-include(${CMAKE_CURRENT_LIST_DIR}/FetchCPM.cmake)
-include(${CMAKE_CURRENT_LIST_DIR}/cmake/CPM.cmake)
-CPMAddPackage(NAME wxWidgets
-    GITHUB_REPOSITORY wxWidgets/wxWidgets
-    VERSION 3.2.0
-    GIT_TAG v3.2.0    
-    OPTIONS
-        "wxBUILD_SHARED FALSE"
-        "wxBUILD_OPTIMISE ON"
-    )
+    set(CPM_SOURCE_CACHE ${CMAKE_CURRENT_LIST_DIR}/third_party/CPM)
+    include(${CMAKE_CURRENT_LIST_DIR}/FetchCPM.cmake)
+    include(${CMAKE_CURRENT_LIST_DIR}/cmake/CPM.cmake)
+    CPMAddPackage(NAME wxWidgets
+        GITHUB_REPOSITORY wxWidgets/wxWidgets
+        VERSION 3.2.0
+        GIT_TAG v3.2.0
+        NO_CACHE TRUE
+        OPTIONS 
+            "wxBUILD_SHARED FALSE"
+            "wxBUILD_OPTIMISE ON"
+        )
+endif()
